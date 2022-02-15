0f38f2b8b1b440815b8f15d67dd2e662729a6de6
irlaandrade <paula.irla@gmail.com>
2022-02-15 07:12:57 -0300

    add mentoria and curso class

--- src/main/java/com/bootcampDesafio/desafio/DesafioApplication.java
+++ src/main/java/com/bootcampDesafio/desafio/DesafioApplication.java
@@ -1,13 +1,25 @@
 package com.bootcampDesafio.desafio;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.bootcampDesafio.desafio.dominio.Curso;
+import com.bootcampDesafio.desafio.dominio.Mentoria;
 
-@SpringBootApplication
+import java.time.LocalDate;
+
 public class DesafioApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(DesafioApplication.class, args);
+		Curso curso1 = new Curso();
+
+		curso1.setTitulo("Curso Java");
+		curso1.setDescricao("Descrição do curso Java");
+		curso1.setCargaHoraria(8);
+
+		Mentoria mentoria1 = new Mentoria();
+		mentoria1.setTitulo("Mentoria de Java");
+		mentoria1.setDescricao("Descrição da mentoria de Java");
+		mentoria1.setData(LocalDate.now());
+
+		System.out.println(curso1);
+		System.out.println(mentoria1);
 	}
 
 }
